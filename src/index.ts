24747f7cdc0a4f3e0f4f93f2a78ba2fe2059a335
Jonathan_Norlin <jonathan.norlin@icloud.com>
2025-04-06 02:03:51 +0200

    revert

--- src/index.ts
+++ src/index.ts
@@ -34,7 +34,10 @@ app.use('/auth', authRouter)
 // Attempt to connect to the database
 connectDB()
 // Start Express server
-
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`The server is running at http://localhost:${PORT}`);
+})
 
 export default (req: VercelRequest, res: VercelResponse) => {
   return app(req, res);
